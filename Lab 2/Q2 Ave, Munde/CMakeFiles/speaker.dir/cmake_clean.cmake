897401ee18abab42f4f79d3f9ea56ecebd43d66a
brookie chookie <you@example.com>
2020-09-17 22:17:21 +1000

    Workings from 17.9.20

---
+++ Lab 2/Q2 Ave, Munde/CMakeFiles/speaker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/speaker.dir/main.cpp.o"
+  "speaker.pdb"
+  "speaker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/speaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
