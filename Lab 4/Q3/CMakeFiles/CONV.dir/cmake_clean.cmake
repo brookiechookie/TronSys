168d47a4591b1b8636b2788b87250355134b2334
brookie chookie <you@example.com>
2020-10-25 22:50:45 +1100

    <Q3> Copied over from lab 1 q3 and compiling

---
+++ Lab 4/Q3/CMakeFiles/CONV.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CONV.dir/main.cpp.o"
+  "CMakeFiles/CONV.dir/Conveyor.cpp.o"
+  "CMakeFiles/CONV.dir/LoadingRobot.cpp.o"
+  "CMakeFiles/CONV.dir/ProcessingRobot.cpp.o"
+  "CONV.pdb"
+  "CONV"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CONV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
